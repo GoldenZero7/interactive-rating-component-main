c62f1a7a268e56386fc3e5b0da86d4338d2dbed6
GoldenZero7 <52763835+GoldenZero7@users.noreply.github.com>
2022-05-09 16:37:17 -0700

    Finished model

--- page1/app.js
+++ page1/app.js
@@ -2,13 +2,15 @@ const buttonClick = document.querySelectorAll(".btn");
 const buttonSubmit = document.getElementById("myButton");
 const thankYou = document.querySelector('.container')
 const ratingScore = document.querySelector('.experance')
-
+const span = document.querySelector('span')
 
  buttonSubmit.addEventListener("click", changeSubmit)
 
 for (var i = 0; i < buttonClick.length; i++) {
   const buttons = buttonClick[i].addEventListener("click", function (e) {
     this.classList.toggle("btnClicked");
+    const rating = e.target.textContent
+    span.textContent = rating
   });
 }
 
