9a7f201ca66b2a7f7a61f6299877dffbe0aa8059
GoldenZero7 <52763835+GoldenZero7@users.noreply.github.com>
2022-05-09 16:27:08 -0700

    Working model 5-9-22

--- page1/app.js
+++ page1/app.js
@@ -1,14 +1,21 @@
+const buttonClick = document.querySelectorAll(".btn");
+const buttonSubmit = document.getElementById("myButton");
+const thankYou = document.querySelector('.container')
+const ratingScore = document.querySelector('.experance')
 
 
+ buttonSubmit.addEventListener("click", changeSubmit)
 
-const buttonClick = document.querySelectorAll('.btn')
+for (var i = 0; i < buttonClick.length; i++) {
+  const buttons = buttonClick[i].addEventListener("click", function (e) {
+    this.classList.toggle("btnClicked");
+  });
+}
 
-
-for (var i = 0; i <buttonClick.length; i++) {
-
-const buttons = buttonClick[i].addEventListener("click", function() {
-        this.classList.toggle('btnClicked')
-});
+function changeSubmit() {
+    thankYou.classList.remove("hidden");
+    ratingScore.classList.add("hidden")
 
 }
- 
+
+
