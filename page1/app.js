26123eb86d680bd963ff86d3e100e304d2585af8
GoldenZero7 <52763835+GoldenZero7@users.noreply.github.com>
2022-05-09 11:04:53 -0700

    Update 5-9-22

---
+++ page1/app.js
@@ -0,0 +1,13 @@
+
+
+
+
+
+// document.querySelector(".btn2").style.color = "purple";
+for (var i = 0; i <document.querySelectorAll(".btn").length; i++) {
+
+const buttons = document.querySelectorAll(".btn")[i].addEventListener("click", function() {
+    console.log("i got clicked")
+});
+
+}
